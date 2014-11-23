de92ad250e4f0c79ef1098dc016a03605002304a
David Baird <david.baird@colorado.edu>
2014-11-23 16:31:39 -0500

    Beginnings of how we will move baxter based on messages.

---
+++ moveBaxter.py
@@ -0,0 +1,19 @@
+#! /usr/bin/python
+
+
+import rospy
+import roslib
+import baxter_interface
+roslib.load_manifest('joint_position')
+
+
+
+
+def main():
+    print("Initializing node...")
+    rospy.init_node("hot_shot_move_robot")
+    print("Getting robot state...")
+    rs = baxter_interface.RobotEnable()
+    print("Enabling robot...")
+    rs.enable()
+
