16a3f300411022f0e586a92b2bc866db49d1b851
Nathan Bellowe <nbellowe@gmail.com>
2014-12-03 23:55:39 -0700

    HotShot ros package setup

---
+++ README.md
@@ -0,0 +1,20 @@
+# Installation
+
+	$ sudo apt-get install ros-indigo-visp
+
+## Get the source
+	# mkdir ~/catkin_ws/src
+	$ cd ~/catkin_ws/src
+	$ git clone https://github.com/nbellowe/HotShot.git
+
+	$ source ~/catkin_ws/devel/setup.bash
+
+## Build the catkin packages from source
+
+	$ cd ~/catkin_ws
+	$ catkin_make 
+
+# Usage
+
+## Start the visual servoing launch script
+	$ roslaunch demo_pioneer hotshot.launch
---
+++ src/pseudo.py
@@ -0,0 +1,31 @@
+# make sure baxter is enabled: 
+# $ rosrun baxter_tools enable_robot.py -e
+
+import rospy
+import baxter_interface
+from std_msgs.msg import Point
+from pid import PID
+
+limb = baxter_interface.Limb('right')
+yaw_pid = PID()
+pitch_pid = PID()
+
+def process_imagelocation(data):
+    rospy.loginfo("Goal heard (x,y):", data.x, data.y)
+    yaw_pid.setPoint(current_value=data.x)
+    pitch_pid.setPoint(current_value=data.y)
+
+def process_centerupdate(data):
+	angles = limb.joint_angles()
+    rospy.loginfo("Center heard x %d, y %d" % data.x, data.y)
+    angles['right_s0'] = yaw_pid.update(current_value=data.x)
+    angles['right_s1'] = pitch_pid.update(current_value=data.y)
+    limb.move_to_joint_positions(angles)
+ 
+def listener():
+    rospy.init_node('baxter_controller')
+    rospy.Subscriber("/hotshot_center", Point, process_centerupdate)
+    rospy.Subscriber("/hotshot_goal", Point, process_imagelocation)
+
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
