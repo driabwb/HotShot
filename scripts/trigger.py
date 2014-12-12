34137c98ce121f443d7ed72b56ec819335414153
Nathan Bellowe <nbellowe@gmail.com>
2014-12-12 00:42:28 -0700

    Update trigger.py

--- scripts/trigger.py
+++ scripts/trigger.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python                                                   
-                                                                            
-import serial                                                
-def fire():
+  	#untested, 10 second adaption ofprior code. I'd assume already tried but worth a shot.
+import rospy
+import serial 
+from std_msgs.msg import Bool
+
+def fire(data):
+	rospy.loginfo("I heard a fire command")
 	port = serial.Serial("/dev/ttyACM0", 9600)                         
 	port.write('f')                                         
 	port.close()
+
+def listener():
+    rospy.init_node('node_name')
+    rospy.Subscriber("/fire_command", Bool, callback)
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+
+
 if __name__ == '__main__':
 	fire()
